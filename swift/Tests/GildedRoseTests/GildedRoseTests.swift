8da355bd0652f20de7c4c10b2aa4e05d2a06d989
Thomas Valera <thomasvalera@gmail.com>
2021-09-26 15:14:34 +0200

    Added failing tests for conjured items

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -25,10 +25,10 @@ class GildedRoseTests: XCTestCase {
         let tests: [(item: Item, sellIn: Int, quality: Int)] = [
             // Standard items
             (Item(name: "Standard", sellIn: 10, quality: 20), 9, 19),
-            (Item(name: "Standard last day", sellIn: 1, quality: 20), 0, 19),
-            (Item(name: "Standard at sellIn", sellIn: 0, quality: 20), -1, 18),
-            (Item(name: "Standard past sellIn", sellIn: -3, quality: 20), -4, 18),
-            (Item(name: "Standard zero quality", sellIn: -3, quality: 0), -4, 0),
+            (Item(name: "Standard", sellIn: 1, quality: 20), 0, 19), // last day
+            (Item(name: "Standard", sellIn: 0, quality: 20), -1, 18), // just passed
+            (Item(name: "Standard", sellIn: -3, quality: 20), -4, 18), // passed
+            (Item(name: "Standard", sellIn: -3, quality: 0), -4, 0), // passed minimum quality
             
             // Aged Brie
             (Item(name: "Aged Brie", sellIn: 2, quality: 0), 1, 1), // default
@@ -53,6 +53,29 @@ class GildedRoseTests: XCTestCase {
             (Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 23), 0, 26), // inside 5-0 days range
             (Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 24), -1, 0), // just passed
             (Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -10, quality: 25), -11, 0), // passed
+        ]
+        
+        let items = tests.map { test in
+            test.item
+        }
+        
+        let gildedRose = GildedRose(items: items)
+        gildedRose.updateQuality()
+        
+        tests.forEach { test in
+            XCTAssertEqual(test.item.sellIn, test.sellIn, "Failed sellIn \(test.item.name). Expected: \(test.sellIn), received: \(test.item.sellIn)")
+            XCTAssertEqual(test.item.quality, test.quality, "Failed quality \(test.item.name). Expected: \(test.quality), received: \(test.item.quality)")
+        }
+    }
+    
+    func testUpdateQuality_conjured() {
+        let tests: [(item: Item, sellIn: Int, quality: Int)] = [
+            // Standard items
+            (Item(name: "Conjured Mana Cake", sellIn: 10, quality: 20), 9, 18),
+            (Item(name: "Conjured Mana Cake", sellIn: 1, quality: 20), 0, 18), // last day
+            (Item(name: "Conjured Mana Cake", sellIn: 0, quality: 20), -1, 16), // just passed
+            (Item(name: "Conjured Mana Cake", sellIn: -3, quality: 20), -4, 16), // passed
+            (Item(name: "Conjured Mana Cake", sellIn: -3, quality: 0), -4, 0), // passed minimum quality
         ]
         
         let items = tests.map { test in
